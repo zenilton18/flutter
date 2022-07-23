620dc4e1e4880ef7a81674347cc8416ceecd1844
Your NameZenilton <ze18.fv@gmail.com>
2022-07-23 12:42:33 -0300

    começo

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+      title: "contador de  Pessoas ",
+      home: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+            "pessoas  : 0 ",
+          ),
+         Row(
+           mainAxisAlignment: MainAxisAlignment.center,
+           children: <Widget>[
+             FlatButton(
+               child: Text(
+                 "+1",
+                 style: TextStyle(fontSize: 40.0, color: Colors.white),
+               ),
+               onPressed: ()=>{},
+             ),
+             FlatButton(
+               child: Text(
+                 "-1",
+                 style: TextStyle(fontSize: 40.0, color: Colors.white),
+               ),
+               onPressed: ()=>{},
+             ),
+           ]
+
+         ),
+          Text(
+            "Pode Entrar!",
+            style: TextStyle(color: Colors.white, fontStyle: FontStyle.italic),
+          ),
+        ],
+      )));
+}
