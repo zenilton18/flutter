92a232b0dba16b8f45c8374cecc9d48724f67e43
Your NameZenilton <ze18.fv@gmail.com>
2022-07-27 18:39:50 -0300

    app_1

---
+++ lib/myapp.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import 'home_page.dart';
+
+class Myapp extends StatelessWidget {
+  const Myapp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
