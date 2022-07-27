44930d92fa97de2c85a66b68f9df1f675c8532bf
Your NameZenilton <ze18.fv@gmail.com>
2022-07-27 17:47:35 -0300

    criando arquivos

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,6 @@
 import 'package:flutter/material.dart';
+import 'myapp.dart';
 
 void main() {
   runApp(const Myapp());
-}
-
-class Myapp extends StatelessWidget {
-  const Myapp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: AppBar(
-        title: Text('home'),
-      ),
-      body: const Center(
-        child: Text('fluterando'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
-        onPressed: () {
-          print("clicou");
-        },
-      ),
-    );
-  }
 }
